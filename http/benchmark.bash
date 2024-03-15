8d95f6ff0730f579601fb2459d2aec4736de63a6
James Orson <jamesaorson@gmail.com>
2024-03-15 08:44:41 -0700

    http: initial with Giraffe (F#) and raw Golang

---
+++ http/benchmark.bash
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+set -euox pipefail
+
+cd $(dirname ${BASH_SOURCE[0]})
+
+PLATFORM=$(uname -s)
+case ${PLATFORM} in
+    Linux)
+        WRK=$(realpath ${WRK:-../bin/linux_amd64/wrk})
+        ;;
+    Darwin)
+        WRK=$(realpath ${WRK:-../bin/osx_arm/wrk})
+        ;;
+    *)
+        echo "Unsupported platform: ${PLATFORM}"
+        exit 1
+        ;;
+esac
+
+show_help() {
+    echo "Usage: ./benchmark.bash <path_to_benchmark>"
+    echo "Benchmark the given HTTP implementation"
+}
+
+if [[ $# -eq 0 ]]; then
+    show_help
+    exit 1
+fi
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h)
+            show_help
+            exit 0
+            ;;
+        --help)
+            show_help
+            exit 0
+            ;;
+        *)
+            pushd $1
+            language=$(basename $1)
+            echo "[${language}] Benchmarking..."
+            make run
+            sleep 5
+            ${WRK} -t12 -c400 -d10s http://127.0.0.1:8080
+            echo "[${language}] Benchmarking done!"
+            set +u
+            if [[ -z ${GITHUB_RUN_ID} ]]; then
+                # NOTE: do not do this in action, as it will kill the action itself
+                pkill -f ${language} > /dev/null
+            fi
+            set -u
+            popd
+            shift
+    esac
+done
---
+++ http/giraffe/Program.fs
@@ -0,0 +1,40 @@
+﻿open System
+open Giraffe
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+
+let webApp = route "/" >=> text "hello"
+
+let configureApp (app : IApplicationBuilder) =
+    app.UseGiraffe webApp
+
+let configureLogging (builder : ILoggingBuilder) =
+    // Set a logging filter (optional)
+    let filter (l : LogLevel) = l.Equals LogLevel.Error
+
+    // Configure the logging factory
+    builder.AddFilter(filter) // Optional filter
+           .AddConsole()      // Set up the Console logger
+           .AddDebug()        // Set up the Debug logger
+    |> ignore
+
+let configureServices (services : IServiceCollection) =
+    services.AddGiraffe() |> ignore
+
+[<EntryPoint>]
+let main _ =
+    Host.CreateDefaultBuilder()
+        .ConfigureWebHostDefaults(
+            fun webHostBuilder ->
+                webHostBuilder
+                    .Configure(configureApp)
+                    .ConfigureLogging(configureLogging)
+                    .ConfigureServices(configureServices)
+                    .UseUrls("http://localhost:8080")
+                    |> ignore)
+        .Build()
+        .Run()
+    0
