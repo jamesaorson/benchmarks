32dc5391409e8e7ea4eee39b1e0fb577f11e99a8
James Orson <jamesaorson@gmail.com>
2024-03-16 20:24:59 -0700

    http: Add babashka

---
+++ http/babashka/http.clj
@@ -0,0 +1,32 @@
+#!/usr/bin/env bb
+
+(require '[babashka.deps :as deps])
+(deps/add-deps '{:deps {org.clojars.askonomm/ruuter {:mvn/version "1.3.2"}}})
+
+(require '[org.httpkit.server :as srv]
+         '[clojure.java.browse :as browse]
+         '[ruuter.core :as ruuter])
+
+(def port 8080)
+
+(defn render-text [text & [status]]
+  {:status (or status 200)
+   :body text})
+
+(defn route-index [{:keys [query-string headers]}]
+  (render-text "hello"))
+
+(def routes [{:path     "/"
+              :method   :get
+              :response route-index}])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Server
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(when (= *file* (System/getProperty "babashka.file"))
+  (let [url (str "http://localhost:" port "/")]
+    (srv/run-server #(ruuter/route routes %) {:port port})
+    (println "serving" url)
+    (browse/browse-url url)
+    @(promise)))
